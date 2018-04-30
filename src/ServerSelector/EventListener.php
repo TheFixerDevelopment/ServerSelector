e9e7a282cc35ccd74b925dc659af00059591d4aa
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-30 13:52:16 +0100

    EventListener: FIx class issues.

--- src/ServerSelector/EventListener.php
+++ src/ServerSelector/EventListener.php
@@ -14,6 +14,7 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat;
 
 class EventListener implements Listener {
 
