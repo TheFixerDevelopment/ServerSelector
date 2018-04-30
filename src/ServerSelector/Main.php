6ab4dbbbdd609e2528c5e888de01630095c31652
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-30 16:56:02 +0100

    Update Main.php

--- src/ServerSelector/Main.php
+++ src/ServerSelector/Main.php
@@ -80,7 +80,7 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
 		$player->setFood(20);
        	$player->setHealth(20);
        	$player->setGamemode(0);
-       	$player->getInventory()->setItem(4, Item::get(345)->setCustomName("§a§lServer Selector! (§bTap me!)"));
+       	$player->getInventory()->setItem(4, Item::get(345)->setCustomName("§a§lServer Selector!\n§5Select a server!\n(§bTap me!)"));
 		$ev->setJoinMessage("");
 	}
         public function onQuit(PlayerQuitEvent $ev) {	
@@ -91,12 +91,12 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
       public function onInteract(PlayerInteractEvent $ev) {
 	   $player = $ev->getPlayer();
        $item = $ev->getItem();
-          if($item->getCustomName() == "§a§lServer Selector! (§bTap me!)"){
+          if($item->getCustomName() == "§a§lServer Selector!\n§5Select a server!\n(§bTap me!)"){
               
             $player->getInventory()->clearAll();
             $player->getInventory()->setSize(9);
-            $player->getInventory()->setItem(0, Item::get(46)->setCustomName("§6Void§bFactions§cPE\n§5Go against the other clans, make your own, invite your loyal members to your clan\n§5Raid people, and more!\n§tYour goal is Beating the other factions!\n(§dTap me!)"));
-            $player->getInventory()->setItem(2, Item::get(276)->setCustomName("§6Void§bKitPvP§cPE\n§5PvP with kits, 1v1 against other players, make your way to the top of the leaderboards, and more!\n(§dTap me!)"));
+            $player->getInventory()->setItem(0, Item::get(46)->setCustomName("§6Void§bFactions§cPE\n§5Go against the other clans\n§5make your own\n§5invite your loyal members to your clan\n§5Raid people, and more!\n§tYour goal is Beating the other factions!\n(§dTap me!)"));
+            $player->getInventory()->setItem(2, Item::get(276)->setCustomName("§6Void§bKitPvP§cPE\n§5PvP with kits\n§51v1 against other players\n§5make your way to the top of the leaderboards, and more!\n(§dTap me!)"));
             $player->getInventory()->setItem(4, Item::get(101)->setCustomName("§6Void§bPrisons§cPE\n§5You're in a prison, you have to mine to rankup.\n§5Your goal is to rank all the way up to Z!\n§5There's also PvP mines as well!\n(§dTap me!)"));
             $player->getInventory()->setItem(6, Item::get(322)->setCustomName("§6Void§bHCF§cPE\n§5HCF = HardCoreFactions. It's factions, but hardcore, which means it's harder.\n§5When you die, you get death banned.\n§5Everything is hardcore mode = More challenging\n§dComing Soon"));
             
