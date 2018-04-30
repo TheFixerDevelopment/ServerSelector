40f5b90b9ed74f4c3a9c43eb09d505ce7387a729
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-30 13:27:44 +0100

    Fixed syntax error.

--- src/ServerSelector/Main.php
+++ src/ServerSelector/Main.php
@@ -24,28 +24,25 @@ use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\Player;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener{
 	
 public function onEnable(): void {
     $this->getLogger()->info("Plugin has been enabled.");
     $this->getServer()->getNetwork()->setName(TextFormat::BOLD . TextFormat::GREEN . "§6§lVoid§bMiner§cPE §dNetwork");
 }
-public function onDisable(): void {
-    $this->getLogger()->info("Plugin has been disabled.");
-}
-public function onPickup(InventoryPickupItemEvent $ev) {
+public function onPickup(InventoryPickupItemEvent $ev){
 		$ev->setCancelled(true);
 }
-public function ItemMove(PlayerDropItemEvent $ev) {
+public function ItemMove(PlayerDropItemEvent $ev){
         $ev->setCancelled(true);
 }
- public function onConsume(PlayerItemConsumeEvent $ev) {
+ public function onConsume(PlayerItemConsumeEvent $ev){
         $ev->setCancelled(true);
 }
-public function onHunger(PlayerExhaustEvent $ev) {
+public function onHunger(PlayerExhaustEvent $ev){
 	$ev->setCancelled(true);
 }
-public function onPreLogin(PlayerPreLoginEvent $ev) {
+public function onPreLogin(PlayerPreLoginEvent $ev){
 		$player = $ev->getPlayer();
 		$name = $player->getName();
 		$ip = $player->getAddress();
@@ -61,41 +58,40 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
 		$player->getInventory()->setSize(9);
 		$player->getInventory()->setItem(0, Item::get(345)->setCustomName("§a§lServer Selector (§bTap me!)"));
         }
-      public function noInvMove(InventoryTransactionEvent $ev) {
+      public function noInvMove(InventoryTransactionEvent $ev){
 		$ev->setCancelled(true);
 	}
-	public function onDamage(EntityDamageEvent $ev) {
+	public function onDamage(EntityDamageEvent $ev){
       		if($ev->getCause() === EntityDamageEvent::CAUSE_FALL){
           	$ev->setCancelled(true);
         	}
     	}
-	public function onPlace(BlockPlaceEvent $ev) {
+	public function onPlace(BlockPlaceEvent $ev){
 			$player = $ev->getPlayer();
 			$ev->setCancelled(true);
 	}
-	public function onBreak(BlockBreakEvent $ev) {
+	public function onBreak(BlockBreakEvent $ev){
 			$player = $ev->getPlayer();
 			$ev->setCancelled(true);
 	}
-	public function onJoin(PlayerJoinEvent $ev) {
+	public function onJoin(PlayerJoinEvent $ev){
 		$player = $ev->getPlayer();
 		$player->getInventory()->clearAll();
 		$player->setFood(20);
-       		$player->setHealth(20);
-       		$player->setGamemode(0);
+       	$player->setHealth(20);
+       	$player->setGamemode(0);
 		$player->getInventory()->setSize(9);
-       		$player->getInventory()->setItem(2, Item::get(345)->setCustomName("§a§lServer Selector! (§bTap me!)"));
+       	$player->getInventory()->setItem(2, Item::get(345)->setCustomName("§a§lServer Selector! (§bTap me!)"));
 		$ev->setJoinMessage("");
 	}
-        public function onQuit(PlayerQuitEvent $ev) {	
+        public function onQuit(PlayerQuitEvent $ev){	
         	$player = $ev->getPlayer();
         	$name = $player->getName();
         	$ev->setQuitMessage("");
 	}
-
-      public function onInteract(PlayerInteractEvent $ev) {
+      public function onInteract(PlayerInteractEvent $ev){
 	   $player = $ev->getPlayer();
-      	`  $item = $ev->getItem();
+       $item = $ev->getItem();
           if($item->getCustomName() == "§a§lServer Selector! (§bTap me!)"){
               
             $player->getInventory()->clearAll();
@@ -105,16 +101,15 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
             $player->getInventory()->setItem(2, Item::get(101)->setCustomName("§6Void§bPrisons§cPE (§dTap me!)"));
             $player->getInventory()->setItem(3, Item::get(322)->setCustomName("§6Void§bHCF§cPE - §dComing Soon"));
             
-          }elseif($item->getCustomName() == "§6Void§bFactions§cPE (§dTap me!)") {
+          }elseif($item->getCustomName() == "§6Void§bFactions§cPE (§dTap me!)"){
 			$ev->getPlayer()->transfer("voidfactionspe.ml", "19132");
 	  
-      }elseif($item->getCustomName() == "§6Void§bPrisons§cPE (§dTao me!)") {
+      }elseif($item->getCustomName() == "§6Void§bPrisons§cPE (§dTao me!)"){
 			$ev->getPlayer()->transfer("voidprisonspe.ml", "25641");
-
-      }elseif($item->getCustomName() ==  "§6Void§bKitPvP§cPE (§dTap me!)") {
+      }elseif($item->getCustomName() ==  "§6Void§bKitPvP§cPE (§dTap me!)"){
 			$ev->getPlayer()->transfer("voidkitpvppe.ml", "25625");
 		  
-      }elseif($item->getCustomName() == "§6Void§bHCF§cPE (§dComing Soon!)") {
+      }elseif($item->getCustomName() == "§6Void§bHCF§cPE (§dComing Soon!)"){
 		  	$ev->getPlayer()->transfer("voidhcfpe.ml", "25630");
 		}
 	    return true;
