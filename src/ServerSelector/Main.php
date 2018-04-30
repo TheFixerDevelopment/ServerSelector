dda3d9ce5c7d2c03d1be8d276e09b9318e88a722
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-30 12:46:58 +0100

    Major revamp - Fixes, and improvements.

--- src/ServerSelector/Main.php
+++ src/ServerSelector/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ServerSelector;
 
 use pocketmine\item\Item;
@@ -20,61 +21,35 @@ use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\event\inventory\InventoryPickupItemEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\Player;
 
-class Main extends PluginBase implements Listener
-{
-  
-public $event;
+class Main extends PluginBase implements Listener{
 	
-public function onEnable(): void
-{
+public function onEnable(): void{
     $this->getLogger()->info("Plugin has been enabled.");
     $this->getServer()->getNetwork()->setName(TextFormat::BOLD . TextFormat::GREEN . "§6§lVoid§bMiner§cPE §dNetwork");
 }
-public function onPickup(InventoryPickupItemEvent $event)
-{
+public function onPickup(InventoryPickupItemEvent $event){
 		$player = $event->getInventory()->getHolder();
 		$defaultLevel = $this->getServer()->getDefaultLevel();
 		$event->setCancelled();
 }
-public function onDrop(PlayerDropItemEvent $event)
-{
-		$player = $event->getPlayer();
-		$defaultLevel = $this->getServer()->getDefaultLevel();
-		$event->setCancelled();
+public function ItemMove(PlayerDropItemEvent $event){
+        $event->setCancelled(true);
 }
-public function onHunger(PlayerExhaustEvent $event)
-{
+ public function onConsume(PlayerItemConsumeEvent $event){
+        $ev->setCancelled(true);
+}
+public function onHunger(PlayerExhaustEvent $event){
 	$event->setCancelled(true);
 }
-public function getSelector(Player $player)
-{
-    $inv = $player->getInventory();
-    $inv->clearAll();
-		$exit = Item::get(351, 1, 1);
-		$exit->setCustomName(TextFormat::RESET . TextFormat::RED . "Exit");
-		$Factions = Item::get(46, 1, 1);
-		$Factions->setCustomName(TextFormat::RESET . TextFormat::BLUE . "§aFactions §5(Tap Me!)");
-		$Prisons = Item::get(101, 1, 1);
-		$Prisons->setCustomName(TextFormat::RESET . TextFormat::GREEN . "§bPrisons §5(Tap Me!)");
-		$KitPvP = Item::get(276, 1, 1);
-		$KitPvP->setCustomName(TextFormat::RESET . TextFormat::GOLD . "§cKitPvP - §5(Tap me!)");
-		$HCF = Item::get(322, 1, 1);
-		$HCF->setCustomName(TextFormat::RESET . TextFormat::RED . "§d§lHCF §5(Tap Me!)");
-		$inv->setItem(8, $exit);
-		$inv->setItem(0, $Factions);
-		$inv->setItem(2, $Prisons);
-		$inv->setItem(4, $KitPvP);
-		$inv->setItem(6, $HCF);
-}
-public function onPreLogin(PlayerPreLoginEvent $event)
-{
+public function onPreLogin(PlayerPreLoginEvent $event){
 		$player = $event->getPlayer();
 		$name = $player->getName();
 		$ip = $player->getAddress();
 		$cid = $player->getClientId();
-	        $in = $event->getPlayer()->getInventory->getItemInHand()->getCustomName();
+	
 		if (!$player->isWhitelisted($name)) {
 			$msg =
 				TextFormat::BOLD . TextFormat::GRAY . "+++-----------+++-----------+++\n" .
@@ -82,83 +57,64 @@ public function onPreLogin(PlayerPreLoginEvent $event)
 				TextFormat::GOLD . "Try again soon";
 			$player->close("", $msg);
 		}
-                if($in == TextFormat::RESET . TextFormat::GOLD . "§b§lServer Selector (§cTap me!)") {
-			
-			$this->getSelector($player);
-                }
-	}
-public function onHit(EntityDamageEvent $event)
-{
-		$entity = $event->getEntity();
-		if ($entity instanceof Player) {
-			if ($event instanceof EntityDamageByEntityEvent) {
-				$damager = $event->getDamager();
-				if ($damager instanceof Player) {
-					if ($entity->getLevel()->getFolderName() == $this->getServer()->getDefaultLevel()->getFolderName()) {
-						$event->setCancelled();
-					}
-				}
-			}
-		}
-	}
-      public function getItems(Player $player)
-      {
-		$name = $player->getName();
-		$inv = $player->getInventory();
-		$inv->clearAll();
-		$item = Item::get(345, 0, 1);
-		$item->setCustomName(TextFormat::RESET . TextFormat::GOLD . "§b§lServer Selector (§cTap me!)");
-      }
-      public function noInvMove(InventoryTransactionEvent $event)
-      {
+		$player->getInventory()->setSize(9);
+		$player->getInventory()->setItem(0, Item::get(345)->setCustomName("§a§lServer Selector (§bTap me!)"));
+        }
+      public function noInvMove(InventoryTransactionEvent $event){
 		$event->setCancelled(true);
 	}
-	public function onDamage(EntityDamageEvent $event)
-	{
-		$player = $event->getEntity();
-		if ($player->getLevel()->getFolderName() == $this->getServer()->getDefaultLevel()->getFolderName()) {
-			if ($player instanceof Player) {
-				$event->setCancelled();
-			}
-		}
+	public function onDamage(EntityDamageEvent $event){
+      		if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
+          	$event->setCancelled(true);
+        	}
+    	}
+	public function onPlace(BlockPlaceEvent $event){
+			$player = $event->getPlayer();
+			$event->setCancelled(true);
 	}
-	public function onPlace(BlockPlaceEvent $event)
-	{
-			$event->setCancelled();
+	public function onBreak(BlockBreakEvent $event){
+			$player = $event->getPlayer();
+			$event->setCancelled(true);
 	}
-	public function onBreak(BlockBreakEvent $event)
-	{
-			$event->setCancelled();
-	}
-	public function onJoin(PlayerJoinEvent $event)
-	{
+	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
-		$name = $player->getName();
-		$this->getItems($player);
-		
+		$player->getInventory()->clearAll();
+		$player->setFood(20);
+       	$player->setHealth(20);
+       	$player->setGamemode(0);
+		$player->getInventory()->setSize(9);
+       	$player->getInventory()->setItem(2, Item::get(345)->setCustomName("§a§lServer Selector! (§bTap me!)"));
 		$event->setJoinMessage("");
-		$event->getPlayer()->setFood("20");
-		$player->setGamemode(0);
-		
-		//$this->getItems($player);
 	}
-        public function onQuit(PlayerQuitEvent $event)
-	{	
-		$event->setQuitMessage("");
+        public function onQuit(PlayerQuitEvent $event){	
+        	$player = $ev->getPlayer();
+        	$name = $player->getName();
+        	$event->setQuitMessage("");
 	}
 
-      public function onInteract(PlayerInteractEvent $event)
-      {
-          $player = $event->getPlayer();
-          $in = $event->getPlayer()->getInventory()->getItemInHand()->getCustomName();
-          if ($in == TextFormat::RESET . TextFormat::BLUE . "Factions") {
+      public function onInteract(PlayerInteractEvent $event){
+	   $player = $event->getPlayer();
+       $item = $event->getItem();
+          if($item->getCustomName() == "§a§lServer Selector! (§bTap me!)"){
+              
+            $player->getInventory()->clearAll();
+            $player->getInventory()->setSize(9);
+            $player->getInventory()->setItem(0, Item::get(46)->setCustomName("§6Void§bFactions§cPE (§dTap me!)"));
+            $player->getInventory()->setItem(1, Item::get(276)->setCustomName("§6Void§bKitPvP§cPE (§dTap me!)"));
+            $player->getInventory()->setItem(2, Item::get(101)->setCustomName("§6Void§bPrisons§cPE (§dTap me!)"));
+            $player->getInventory()->setItem(3, Item::get(322)->setCustomName("§6Void§bHCF§cPE - §dComing Soon"));
+            
+          }elseif($item->getCustomName() == "§6Void§bFactions§cPE (§dTap me!)") {
 			$event->getPlayer()->transfer("voidfactionspe.ml", "19132");
-		}
-		if ($in == TextFormat::RESET . TextFormat::GREEN . "Prisons") {
+	  
+      }elseif($item->getCustomName() == "§6Void§bPrisons§cPE (§dTao me!)") {
 			$event->getPlayer()->transfer("voidprisonspe.ml", "25641");
-		}
-		if ($in == TextFormat::RESET . TextFormat::GOLD . "KitPvP") {
-			$event->getPlayer()->transfer("voidkitpvppe.ml", "25630");
+
+      }elseif($item->getCustomName() ==  "§6Void§bKitPvP§cPE (§dTap me!)"){
+			$event->getPlayer()->transfer("voidkitpvppe.ml", "25625");
+		  
+      }elseif($item->getCustomName() == "§6Void§bHCF§cPE (§dComing Soon!)"){
+		  	$event->getPlayer()->transfer("voidhcfpe.ml", "25630");
 		}
 	    return true;
       }
