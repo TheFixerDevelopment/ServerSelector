beae8047a9d4f52b2cbe3c0ab75576973c841e6b
iZeaoGamer <jameshayward14@icloud.com>
2018-05-01 20:12:30 +0100

    Added.

--- src/ServerSelector/Main.php
+++ src/ServerSelector/Main.php
@@ -56,6 +56,7 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
 	        $player->getInventory()->clearAll();
 	        $player->getInventory()->setSize(9);
 		$player->getInventory()->setItem(4, Item::get(345)->setCustomName("§a§lServer Selector (§bTap me!)"));
+		 $player->getInventory()->setItem(2, Item::get(347)->setCustomName("§a§lGames Selector\n§5§lSelect a Game to play!\n(§bTap me!)"));
         }
       public function noInvMove(InventoryTransactionEvent $ev) {
 		$ev->setCancelled(true);
@@ -82,6 +83,7 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
        	$player->setGamemode(0);
         $player->getInventory()->setSize(9);
        	$player->getInventory()->setItem(4, Item::get(345)->setCustomName("§a§lServer Selector!\n§5Select a server!\n(§bTap me!)"));
+	 $player->getInventory()->setItem(2, Item::get(347)->setCustomName("§a§lGames Selector\n§5§lSelect a Game to play!\n(§bTap me!)"));
 		$ev->setJoinMessage("§a$name §6joined the hub.");
 	}
         public function onQuit(PlayerQuitEvent $ev) {	
@@ -100,6 +102,15 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
             $player->getInventory()->setItem(2, Item::get(276)->setCustomName("§6Void§bKitPvP§cPE\n§5PvP with kits\n§51v1 against other players\n§5make your way to the top of the leaderboards, and more!\n(§dTap me!)"));
             $player->getInventory()->setItem(4, Item::get(101)->setCustomName("§6Void§bPrisons§cPE\n§5You're in a prison, you have to mine to rankup.\n§5Your goal is to rank all the way up to Z!\n§5There's also PvP mines as well!\n(§dTap me!)"));
             $player->getInventory()->setItem(6, Item::get(322)->setCustomName("§6Void§bHCF§cPE\n§5HCF = HardCoreFactions. It's factions, but hardcore, which means it's harder.\n§5When you die, you get death banned.\n§5Everything is hardcore mode = More challenging\n§dComing Soon"));
+	  } else {
+          if($item->getCustomName() == "§a§lGames Selector\n§5§lSelect a Game to play!\n(§bTap me!)"){
+     
+	    $player->getInventory()->clearAll();
+	    $player->getInventory()->setSize(9);
+            $player->getInventory()->setItem(1, Item::get(397, 3)->setCustomName("§6Murder§bMystery\n§bThere's one murder, one bystanderd, and everyone else is innocent.\n§aObjectives:\n§bMurder - §3Has to kill everyone to win.\n§cBystanderd - §4Has a gun, they're suppose to kill the murderer to win.\n§dInnocent - §5Make sure you don't get killed.\n§dComing Soon!"));
+            $player->getInventory()->setItem(8, Item::get(261)->setCustomName("§6Sky§bWars\n§bYou're in the sky, you have to:\n§cLoot chests, Get good loot\n§dand kill players!\n§eLast man standing wins!\n§1Coming Soon."));
+            $player->getInventory()->setItem(9, Item::get(260)->setCustomName("§6Survival§bGames\n§bYou're in a survival area, filled with chests\n§cFilled with Loot\n§dYour objective is to\n§eKill the players.\n§1Last man standing wins.\n§2Coming soon."));
+            $player->getInventory()->setItem(3, Item::get(322)->setCustomName("§6U§bH§cC\n§bUHC = UltraHardCore.\n§cYou're in a survival map\n§dbut things get extremly hard.\n§eThere's no: Regeneration\n§1When you die, you get banned\n§2until the game is over.\n§3Coming Soon"));
             
           }elseif($item->getCustomName() == "§6Void§bFactions§cPE\n§5Go against the other clans\n§5make your own\n§5invite your loyal members to your clan\n§5Raid people, and more!\n§tYour goal is Beating the other factions!\n(§dTap me!)"){
 			$ev->getPlayer()->transfer("voidfactionspe.ml", "19132");
@@ -112,7 +123,20 @@ public function onPreLogin(PlayerPreLoginEvent $ev) {
 		  
       }elseif($item->getCustomName() == "§6Void§bHCF§cPE\n§5HCF = HardCoreFactions. It's factions, but hardcore, which means it's harder.\n§5When you die, you get death banned.\n§5Everything is hardcore mode = More challenging\n§dComing Soon"){
 		  	$ev->getPlayer()->transfer("voidhcfpe.ml", "25630");
+		  
+       }elseif($item->getCustomName() == "§6Murder§bMystery\n§bThere's one murder, one bystanderd, and everyone else is innocent.\n§aObjectives:\n§bMurder - §3Has to kill everyone to win.\n§cBystanderd - §4Has a gun, they're suppose to kill the murderer to win.\n§dInnocent - §5Make sure you don't get killed.\n§dComing Soon!"){
+			$player->sendMessage("§c§lNew Game Coming Soon!");
+	  
+      }elseif($item->getCustomName() == "§6Sky§bWars\n§bYou're in the sky, you have to:\n§cLoot chests, Get good loot\n§dand kill players!\n§eLast man standing wins!\n§1Coming Soon."){
+			$player->sendMessage("§c§lNew Game Coming Soon!");
+		  
+      }elseif($item->getCustomName() ==  "§6Survival§bGames\n§bYou're in a survival area, filled with chests\n§cFilled with Loot\n§dYour objective is to\n§eKill the players.\n§1Last man standing wins.\n§2Coming soon."){
+			$player->sendMessage("§c§lNew Game Coming Soon!");
+		  
+      }elseif($item->getCustomName() == "§6U§bH§cC\n§bUHC = UltraHardCore.\n§cYou're in a survival map\n§dbut things get extremly hard.\n§eThere's no: Regeneration\n§1When you die, you get banned\n§2until the game is over.\n§3Coming Soon"){
+		  	$player->sendMessage("§c§lNew Game Coming Soon!");
 		}
 	    return true;
       }
+}
 }
